b8438cd733a0929d01054a8d1287dd5995e6e98f
Joe O'Connell <joconnell@frontlineed.com>
2023-04-07 09:28:29 -0700

    Add square

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mathTools',
-    version='2023.4.7.0',
+    version='2023.4.7.1',
     packages=find_packages('src'),
     package_dir={'':'src'},
     install_requires=[
