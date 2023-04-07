29c9f33e515f790874b82cf8deb226b1c108df4d
Joe O'Connell <joconnell@frontlineed.com>
2023-04-07 10:39:08 -0700

    Added github actions workflow

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="mathTools",
-    version="2023.4.7.1",
+    version="2023.4.7.2",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=["ipython", "black", "pandas", "pytest"],
+    install_requires=["ipython", "black", "pandas", "pytest", "mypy"],
     tests_require=["unittest"],
     test_suite="tests",
 )
