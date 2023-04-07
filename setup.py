76eb5f4613744367a21e7399fd1bc2b348338c4b
Joe O'Connell <joconnell@frontlineed.com>
2023-04-07 10:43:57 -0700

    Updated workflow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mathTools",
-    version="2023.4.7.2",
+    version="2023.4.7.3",
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=["ipython", "black", "pandas", "pytest", "mypy"],
